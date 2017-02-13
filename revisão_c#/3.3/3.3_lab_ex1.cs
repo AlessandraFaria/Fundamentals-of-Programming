d628fbc0511dfc03230a2ba52e191bb0dcc53450
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2017-02-13 20:21:00 -0200

    Update 3.3_lab_ex1.cs

--- revisão_c#/3.3/3.3_lab_ex1.cs
+++ revisão_c#/3.3/3.3_lab_ex1.cs
@@ -12,7 +12,7 @@
 // descricao: recebe uma data em numeros string através da linha de comando,
 // converte para inteiro e armazena em um vetor , imprime a data e chama um método estático que receba estes
 // números inteiros faz os calculos necessrios para saber os dias pecorridos,  e retorna a quandidade
-//de dias pecorridos desde 01/01/2017
+//de dias pecorridos desde 01/01/(ano digitado)
 //
 
 using System;
@@ -27,7 +27,7 @@ namespace Ex33labex1
     {
         static void Main(string[] args)
         {
-            int i, tamanho_args,dia = 0, mes = 0, ano = 0;
+            int i, tamanho_args, dia = 0, mes = 0, ano = 0;
             int[] vetor = new int[3];
             tamanho_args = args.Length;
             for (i = 0; i < tamanho_args; i++)
@@ -41,74 +41,56 @@ namespace Ex33labex1
 
             Console.WriteLine("Desenvolvedores Alessandra Faria Abreu e Igor Santana"); //imprime o nome OBRIGATÓRIO
             Console.WriteLine("Dia = " + dia + " Mês = " + mes + " Ano = " + ano); //imprime o vetor com valores antigos
-            Console.WriteLine("a quantidade de dias pecorridos desde 01/01/2017 é : "+DataDiferenca(dia, mes, ano)+" dias");
+            Console.WriteLine("a quantidade de dias pecorridos desde 01/01/" + ano + "é : " + quantosDias(dia, mes, ano) + " dias");
 
         }
 
-        static double DataDiferenca(int dia, int mes, int ano)
+        static double quantosDias(int dia, int mes, int ano)
         {
             double somadia = 0;
             double somames = 0;
             double somaano = 0;
             double contadias = 0;
-            double[] vetor_dia_mes_bi = {30,29,31,30,31,30,31,31,30,31,30,31};
-            double[] vetor_dia_mes = {31,28,31,29,31,30,29,31,30,31,30,30};
+            double[] vetor_dia_mes_bi = { 30, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            double[] vetor_dia_mes = { 31, 28, 31, 29, 31, 30, 29, 31, 30, 31, 30, 30 };
             int resto = ano % 4;
-
-            if (ano>2016){
+            
                 if (dia != 1)
                 {
                     somadia = dia;
 
                 }
-                if (resto!=0) {
+                if (resto != 0)
+                {
 
                     if (mes != 1)
                     {
-                        for (int i = 0; i < (mes - 1); i++)
+                        for (int i = 0; i < (mes-1); i++)
                         {
                             somames = somames + vetor_dia_mes[i];
                         }
                         somames = somames + somadia;
                         somadia = 0;
                     }
-                    if ((ano != 2017) && (ano > 2017))
-                    {
-                        ano = ano - 2017;
-
-                        somaano = (ano * 365) + somames;
-                        somames = 0;
-                    }
+                    
                 }
                 else
                 {
                     if (mes != 1)
                     {
-                        for (int i = 1; i < (mes - 1); i++)
+                        for (int i = 1; i < mes; i++)
                         {
                             somames = somames + vetor_dia_mes_bi[i];
                         }
                         somames = somames + somadia;
                         somadia = 0;
                     }
-                    if ((ano != 2017) && (ano > 2017))
-                    {
-                        ano = ano - 2017;
-
-                        somaano = (ano * 366) + somames;
-                        somames = 0;
-                    }
+                    
                 }
 
-               contadias = somadia + somames + somaano;
-            }
-
-            else
-            {
-            Console.WriteLine("ano iferior a 2017");
-            contadias=0;
-            }
-           return contadias;
+                contadias = somadia + somames;
+            
+            return contadias;
         }
     }
 }
