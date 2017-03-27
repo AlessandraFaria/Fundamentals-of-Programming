ef7059c366a23e0e1ee40c3b4d9170b0dd6d346d
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2017-03-26 21:31:39 -0300

    Update 2.6_char.cs

--- lab_4/2.6_char.cs
+++ lab_4/2.6_char.cs
@@ -1,3 +1,12 @@
+
+//Programa de teste do sistema de arquivos
+//Alunos Sicrano de tal e Fulano José
+//Arquivo texto de entrada: entrada.txt
+//Arquivo texto de saída: saida.txt
+//Entre com os caracteres: a x r s q w z*
+//Caracteres digitados: a x r s q w z
+//Caracteres convertidos: A X R S Q W Z
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,15 +24,16 @@ namespace copia_texto_2
             nomeEntrada = args[0];
             nomesaida = args[1];
             char[] vetor1 = new char[20];
-            char[] vetor2 = new char[20];
+            string[] vetor2 = new string[20];
             int  i=0;
 
             Stream fileEntrada = File.Open(args[0], FileMode.Open); // abrir arquivo
             Stream fiSaida = File.Open(args[1], FileMode.Create); // criar arquivo
 
+            Console.WriteLine("Programa de teste do sistema de arquivos");
             Console.WriteLine("Alessandra Faria , Alaene ,victor souza , Igor santana ...");
-            //Console.WriteLine("Arquivo de Texto de entrada: " + fileEntrada.Name);
-            //Console.WriteLine("Arquivo de Texto de saída: " + fiSaida.Name);
+            Console.WriteLine("Arquivo de Texto de entrada: " +args[0]);
+            Console.WriteLine("Arquivo de Texto de saída: " + args[1]);
 
             BinaryWriter entrada = new BinaryWriter(fileEntrada);
             BinaryWriter saida = new BinaryWriter(fiSaida);
@@ -31,7 +41,6 @@ namespace copia_texto_2
             Console.WriteLine("Entre com os caracteres:");
             string verificaTexto = Console.ReadLine();
             char textoEscrito = Convert.ToChar(verificaTexto);
-         
             while (verificaTexto != "*")
             {
                 textoEscrito = Convert.ToChar(verificaTexto);
@@ -47,19 +56,26 @@ namespace copia_texto_2
                 verificaTexto = Console.ReadLine();
             }
             entrada.Close();
-
+            Console.Write("Caracteres digitados ");
             for (int j = 0; j < 20; j++)
             {
-                vetor2[j] = vetor1[j];
+                string vetor1String = Convert.ToString(vetor1[j]);
+                string linhaConvertida = vetor1String.ToUpper();
+                vetor2[j] = linhaConvertida;
                 if (vetor1[j] < 0) break; // erro no arquivo
                 if (vetor1[j] == 0) break; // fim de arquivo
                 saida.Write(vetor2[j]);
+                Console.Write(vetor1[j]+ " ");
 
             }
-           
+            Console.WriteLine(" ");
             saida.Close();
 
-
+            Console.Write(" Caracteres convertidos ");
+            for (int k=0;vetor2[k]!=null;k++)
+            {
+                Console.Write(vetor2[k]);
+            }
 
         }
     }
