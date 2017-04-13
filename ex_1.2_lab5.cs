530767a97b7ef0e641a51f56b6fec81ab8e510e5
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2017-04-13 00:07:37 -0300

    Update ex_1.2_lab5.cs

--- ex_1.2_lab5.cs
+++ ex_1.2_lab5.cs
@@ -7,13 +7,14 @@ namespace lab_5._1_ex12
 {
     class Menu
     {
-       // static PFisica chamaPF = new PFisica();
-        //static PJuridica chamaPJ = new PJuridica();
+        const int MAXCONTRIBUINTES = 100; // número máximo de contas suportado
+        static Contribuinte[] lst = new Contribuinte[MAXCONTRIBUINTES]; //vetor de contribuintes
+                                                                        
         static void Main(string[] args)
         {
-            int j = 0 ;
+            int j = 0,k=0 ;
             string opcao, nomeArq, linha;
-            //double venda;
+
             Console.WriteLine("Alessandra Faria 573831");
             Console.WriteLine("Menu");
             Console.WriteLine("1. Incluir um contribuinte.");
@@ -30,24 +31,56 @@ namespace lab_5._1_ex12
                 switch (opcao)
                 {
                     case "1":
-                        Console.WriteLine("digite o nome do arquivo ");
-                        nomeArq = Console.ReadLine();
+                        //Console.WriteLine("digite o nome do arquivo ");
+                        //nomeArq = Console.ReadLine();
 
-                        FileInfo fi = new FileInfo(nomeArq);
-                        StreamReader entrada = new StreamReader(nomeArq); //abrir o arquivo para leitura
+                        //FileInfo fi = new FileInfo(nomeArq);
+                        //StreamReader entrada = new StreamReader(nomeArq); //abrir o arquivo para leitura
 
-                        for ( j = 0; j < fi.Length; j++)
-                        {
-                            linha = entrada.ReadLine();     //ler 1a linha
-                            while (linha != null)           //enquanto houver dados...
+                        //for ( j = 0; j < fi.Length; j++)
+                        //{
+                        //    linha = entrada.ReadLine();     //ler 1a linha
+                        //    while (linha != null)           //enquanto houver dados...
+                        //    {
+                        //        string[] separa = linha.Split(',');
+                        //        linha = entrada.ReadLine(); //ler proxima linha
+                        //        j++;
+                        //    }
+                        // }
+                        Console.WriteLine("Digite o nome do contrinuite");
+                        string nome = Console.ReadLine();
+                        Console.WriteLine("Digite o Endereço do contrinuite");
+                        string endereço = Console.ReadLine();
+                        Console.WriteLine("O contribuite é pessoa fisica(1) ou juridica (2)?");
+                        string op2 = Console.ReadLine();
+                            switch (op2)
                             {
-                                string[] separa = linha.Split(',');
-                                linha = entrada.ReadLine(); //ler proxima linha
-                                j++;
-                            }
-                        }
+                                case 1:
+                                  Console.WriteLine("pessoa fisica");
+                                  Console.WriteLine("qual o valor do salario do contribuinte");
+                                  double salario = Convert.ToDouble(Console.ReadLine());
+                                  Console.WriteLine("qual o numero do cpf  do contribuinte");
+                                  string cpf = Console.ReadLine();
+
+                                lst[k] = new PFisica(nome, 1, endereço,salario,cpf );
+
+                                break;
+                                case 2:
+                                Console.WriteLine("pessoa juridica");
+                                Console.WriteLine("qual o valor do faturamento do contribuinte");
+                                double faturamento = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine("qual o numero do cnpj do contribuinte");
+                                string cnpj = Console.ReadLine();
+                                lst[k] = new PJuridica(nome, 1, endereço, faturamento, cnpj);
+
+                                break;
+                        }   
+                        
+                       
+
 
                         break;
+                        
                 }
                 Console.WriteLine("Alessandra Faria 573831");
                 Console.WriteLine("Menu");
