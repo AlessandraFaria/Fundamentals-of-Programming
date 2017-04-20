ba0d42d1e9fa66e55708d0b1cfcaef11356c9f6d
Alessandra Faria <alessandra.faria.abreu@gmail.com>
2017-04-19 23:10:43 -0300

    Update 4.1_lab_ex3.cs

--- lab_3/4.1_lab_ex3.cs
+++ lab_3/4.1_lab_ex3.cs
@@ -2,220 +2,193 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
-namespace lab_5._1_ex12
-{
-    class Menu
-    {
-        const int MAXCONTRIBUINTES = 100; // número máximo de contas suportado
-        static Contribuinte[] lst = new Contribuinte[MAXCONTRIBUINTES]; //vetor de contribuintes
-        static PFisica chamaPf = new PFisica();
-        static PJuridica chamaPJ = new PJuridica();
+using System.Threading.Tasks;
 
+namespace lab_4._1_ex3
+{
+    public class Funcionario
+    {
+        private string rg;
+        private string nome;
+        public Funcionario() { }
+
+        public Funcionario(String nome, String rg)
+        {//construtor
+            this.nome = nome;
+            this.rg = rg;
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Rg
+        {
+            get { return rg; }
+            set { rg = value; }
+        }
+
+    }
+
+    public class Vendedor : Funcionario
+    {
+        double comicao;
+        private double salarioTotal;
+
+        public Vendedor()
+        {
+            comicao = 0;
+            salarioTotal = 1200;
+        }
+
+        public double CalComi(double venda)
+        {
+            comicao = (venda * 5) / 100;
+            salarioTotal = salarioTotal + comicao;
+            return comicao;
+        }
+
+        public Double SalarioTotal
+        {
+            get { return salarioTotal; }
+            set { salarioTotal = value; }
+        }
+
+    }
+
+    public class Administrativo : Funcionario
+    {
+        double hExtra;
+        double salarioAdmTotal;
+
+        public Administrativo()
+        {
+            salarioAdmTotal = 3000;
+        }
+
+        public double HoraEx(double hora)
+        {
+            hExtra = hora * 0.01;
+            salarioAdmTotal = salarioAdmTotal + hExtra;
+            return hExtra;
+        }
+
+        public Double SalarioAdmTotal
+        {
+            get { return salarioAdmTotal; }
+            set { salarioAdmTotal = value; }
+        }
+    }
+
+    class Program
+    {
+        static Vendedor chamaVendedor = new Vendedor();
+        static Administrativo chamaAdministrador = new Administrativo();
+        //  static Funcionario chamaFuncionario;
+        public static Vendedor[] vetFun = new Vendedor[20];
+        public static Administrativo[] vetFunAdm = new Administrativo[20];
         static void Main(string[] args)
         {
-            int j = 0,k=0 ;
-            string opcao, nomeArq, linha;
+            int i = 0;
+            int j = 0;
+            string opcao;
+            double venda;
 
-            Console.WriteLine("Alessandra Faria 573831");
-            Console.WriteLine("Menu");
-            Console.WriteLine("1. Incluir um contribuinte.");
-            Console.WriteLine("2. Excluir um contribuinte.");
-            Console.WriteLine("3. Exibir os dados de um contribuinte: CPF/CNPJ, nome, endereço e salario/faturamento");
-            Console.WriteLine("4. Calcular e exibir o imposto a ser pago por um contribuinte.");
-            Console.WriteLine("5. Imprimir uma relação dos contribuintes Pessoa Física cadastrados, mostrando os dados:CPF, nome e endereço.");
-            Console.WriteLine("6. Imprimir uma relação dos contribuintes Pessoa Jurídica cadastrados, mostrando os dados: CNPJ, nome e endereço.");
-            Console.WriteLine("7. Sair do programa.");
+            Console.WriteLine("Menu RH");
+            Console.WriteLine("1- Cadastrar funcionário novo");
+            Console.WriteLine("2- Menu Vendedor");
+            Console.WriteLine("3- Menu Administrativo");
+            Console.WriteLine("4- Sair");
             opcao = Console.ReadLine();
-
-            while (opcao != "7")
+            while (opcao != "4")
             {
                 switch (opcao)
                 {
                     case "1":
-                        //Console.WriteLine("digite o nome do arquivo ");
-                        //nomeArq = Console.ReadLine();
-
-                        //FileInfo fi = new FileInfo(nomeArq);
-                        //StreamReader entrada = new StreamReader(nomeArq); //abrir o arquivo para leitura
-
-                        //for ( j = 0; j < fi.Length; j++)
-                        //{
-                        //    linha = entrada.ReadLine();     //ler 1a linha
-                        //    while (linha != null)           //enquanto houver dados...
-                        //    {
-                        //        string[] separa = linha.Split(',');
-                        //        linha = entrada.ReadLine(); //ler proxima linha
-                        //        j++;
-                        //    }
-                        // }
-                        Console.WriteLine("Digite o nome do contrinuite");
-                        string nome = Console.ReadLine();
-                        Console.WriteLine("Digite o Endereço do contrinuite");
-                        string endereço = Console.ReadLine();
-                        Console.WriteLine("O contribuite é pessoa fisica(1) ou juridica (2)?");
+                        Console.Clear();
+                        Console.WriteLine("1- Vendedor 2-Administrativo");
                         string op2 = Console.ReadLine();
-                            switch (op2)
-                            {
-                                case 1:
-                                  Console.WriteLine("pessoa fisica");
-                                  Console.WriteLine("qual o valor do salario do contribuinte");
-                                  double salario = Convert.ToDouble(Console.ReadLine());
-                                  Console.WriteLine("qual o numero do cpf  do contribuinte");
-                                  string cpf = Console.ReadLine();
-
-                                lst[k] = new PFisica(nome , endereço,salario,cpf );
-                                k++;
-                                break;
-
-                                case 2:
-                                Console.WriteLine("pessoa juridica");
-                                Console.WriteLine("qual o valor do faturamento do contribuinte");
-                                double faturamento = Convert.ToDouble(Console.ReadLine());
-                                Console.WriteLine("qual o numero do cnpj do contribuinte");
-                                string cnpj = Console.ReadLine();
-                                lst[k] = new PJuridica(nome, endereço, faturamento, cnpj);
-                                k++;
-
-                                break;
-                        }  
+                        if (op2 == "1")
+                        {
+                            vetFun[i] = new Vendedor();
+                            Console.WriteLine("Digite o nome do novo funcionário");
+                            vetFun[i].Nome = Console.ReadLine();
+                            Console.WriteLine("Digite o RG do novo funcionário");
+                            vetFun[i].Rg = Console.ReadLine();
+                            vetFun[i] = chamaVendedor;
+                            i++;
+                        }
+                        else if (op2 == "2")
+                        {
+                            vetFunAdm[j] = new Administrativo();
+                            Console.WriteLine("Digite o nome do novo funcionário");
+                            vetFunAdm[j].Nome = Console.ReadLine();
+                            Console.WriteLine("Digite o RG do novo funcionário");
+                            vetFunAdm[j].Rg = Console.ReadLine();
+                            j++;
+                        }
+                        else { Console.WriteLine("Opção Inválida"); }
                         break;
-                    case 2:
-                        Console.WriteLine("PF(1) ou PJ (2) ?");
+
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("1- Cadastrar nova venda");
+                        Console.WriteLine("2- Imprimir salário total");
                         string op3 = Console.ReadLine();
-                        switch (op3)
+                        if (op3 == "1")
                         {
-                            case 1:
-                                Console.WriteLine("Dite o Nome ");
-                                string nome3 = Console.ReadLine();
-                                for (int y=0;y<lst.Length;y++)
-                                {
-                                    if (lst[y].getNome==nome3)
-                                    {
-                                       lst[y] = new PFisica(null, null, 0, null);
-                                    }
-                                }
-                                break;
-                            case 2:
-                                Console.WriteLine("Dite o Nome ");
-                                string nome4 = Console.ReadLine();
-                                for (int y = 0; y < lst.Length; y++)
-                                {
-                                    if (lst[y].getNome == nome4)
-                                    {
-                                        lst[y] = new PJuridica(null, null, 0, null);
-                                    }
-                                }
-                                break;
-                        }    
-                        break;
-                    case 3:
-                        Console.WriteLine("PF(1) ou PJ (2) ?");
-                        string op4 = Console.ReadLine();
-                        switch (op4)
+                            Console.Write("Entre com o numero do Vendedor: ");
+                            int numero_Vend = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Digite o valor da venda");
+                            venda = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Comição salva: {0}", vetFun[numero_Vend - 1].CalComi(venda));
+                        }
+                        else if (op3 == "2")
                         {
-                            case 1:
-                                Console.WriteLine("Dite o Nome ");
-                                string nome5 = Console.ReadLine();
-                                for (int o = 0; o < lst.Length; o++)
-                                {
-                                    if (lst[o].getNome == nome3)
-                                    {
-                                        Console.WriteLine(lst[o].getNome);
-                                    }
-                                }
-                                break;
+                            Console.Write("Entre com o numero do Vendedor: ");
+                            int numero_Vend = int.Parse(Console.ReadLine());
+                            Console.WriteLine("O valor total do salário é: {0}", vetFun[numero_Vend - 1].SalarioTotal);
                         }
                         break;
-                        
+
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine("1- Cadastrar Hora Extra");
+                        Console.WriteLine("2- Imprimir salário total");
+                        string op4 = Console.ReadLine();
+                        if (op4 == "1")
+                        {
+                            Console.Write("Entre com o numero do Adiministrador: ");
+                            int numero_Adm = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Digite a quantidade de horas extras");
+                            double hora = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("bônus salvo: {0}", vetFunAdm[numero_Adm - 1].HoraEx(hora));
+                        }
+                        else if (op4 == "2")
+                        {
+                            Console.Write("Entre com o numero do Adiministrador: ");
+                            int numero_Adm = int.Parse(Console.ReadLine());
+                            Console.WriteLine("O valor total do salário é: {0}", vetFunAdm[numero_Adm - 1].SalarioAdmTotal);
+                        }
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção Inválida.");
+                        break;
                 }
-                Console.WriteLine("Alessandra Faria 573831");
-                Console.WriteLine("Menu");
-                Console.WriteLine("1. Incluir um contribuinte.");
-                Console.WriteLine("2. Excluir um contribuinte.");
-                Console.WriteLine("3. Exibir os dados de um contribuinte: CPF/CNPJ, nome, endereço e salario/faturamento");
-                Console.WriteLine("4. Calcular e exibir o imposto a ser pago por um contribuinte.");
-                Console.WriteLine("5. Imprimir uma relação dos contribuintes Pessoa Física cadastrados, mostrando os dados:CPF, nome e endereço.");
-                Console.WriteLine("6. Imprimir uma relação dos contribuintes Pessoa Jurídica cadastrados, mostrando os dados: CNPJ, nome e endereço.");
-                Console.WriteLine("7. Sair do programa.");
+                Console.WriteLine("Menu RH");
+                Console.WriteLine("1- Cadastrar funcionário novo");
+                Console.WriteLine("2- Menu Vendedor");
+                Console.WriteLine("3- Menu Administrativo");
+                Console.WriteLine("4- Sair");
                 opcao = Console.ReadLine();
             }
         }
-       
     }
 
-        public abstract class Contribuinte
-    {
-        protected string nome;
-        protected string endereco;
-        public string getNome()
-        {
-            return nome;
-        }
-        public string getEnd()
-        {
-            return endereco;
-        }
-        abstract public double calcImposto();
 
-}
-    public class PFisica : Contribuinte
-    {
-        protected String cpf;
-        protected double salario;
-        public PFisica(String n, string end, double sal, String c)
-        {
-            cpf = c;
-            salario = sal;
-            base.nome = n;
-            base.endereco = end;
-        }
-        public string getCpf()
-        {
-            return cpf;
-        }
-        public override double calcImposto()
-        {
-            double percentual = 0;
-            if ((salario>0)&&(salario<=1400))
-            {
-                 percentual = 0;
-            }
-            else if((salario>1400)&&(salario<=2100)){
-                percentual = (salario * 0.1)-100;
-            }
-            else if ((salario > 2100) && (salario <= 2800))
-            {
-                percentual = (salario * 0.15)-270;
-            }
-            else if ((salario > 2800) && (salario <= 3600))
-            {
-                percentual = (salario * 0.25)-500;
-            }
-            else
-            {
-                percentual = (salario * 0.3)-700;
-            }
-            return percentual;
-        }
-    }
-    public class PJuridica : Contribuinte
-    {
-        protected String cnpj;
-        protected double faturamento;
-        public PJuridica(String n, string end, double f, String c)
-        {
-            cnpj = c;
-            faturamento = f;
-            base.nome = n;
-            base.endereco = end;
-        }
-        public override double calcImposto()
-        {
-            double percentual = 0.1 * faturamento;
-            return percentual;
-        }
-    }
+
 
 
 }
